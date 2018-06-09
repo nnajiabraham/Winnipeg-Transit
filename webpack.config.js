e468b59c595ee9047579ad55b7de4352ef5a292a
Abraham Nnaji <nnajiabraham@gmail.com>
2018-06-08 19:50:09 -0500

    updates

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./public/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js"
+  }
+  module: {
+     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+
+};
